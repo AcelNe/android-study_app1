261f6b8dc7fd44f537a38f9b36bc62fe2446d43f
MONINGSUN <moningsun1021@gmail.com>
2024-05-23 15:29:42 +0900

    love_test 화면 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
 
     //suspend st
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+
+    //lovetest
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 }
